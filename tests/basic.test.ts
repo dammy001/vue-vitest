a152b9ba73e66ed70eb45f319a888bdd53c9bcdd
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-01-03 18:15:43 +0100

    testing

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,4 +1,4 @@
-import { assert, expect, it, suite, test } from 'vitest';
+import { assert, expect, it, describe, suite, test } from 'vitest';
 
 test('Math.sqrt()', () => {
  assert.equal(Math.sqrt(2), Math.SQRT2);
@@ -12,3 +12,45 @@ hi.test('expect truthy', () => {
  expect(null).not.toBeTruthy();
  expect([]).toBeTruthy();
 });
+
+const person = {
+    isActive: true,
+    age: 32,
+  };
+
+  describe('person', () => {
+    test('person is defined', () => {
+      expect(person).toBeDefined()
+    });
+
+    test('is active', () => {
+      expect(person.isActive).toBeTruthy();
+    });
+
+    test('age limit', () => {
+      expect(person.age).toBeLessThanOrEqual(32);
+    });
+  });
+
+  const numberToCurrency = (value) => {
+    if (typeof value !== 'number') {
+        throw new Error(`Value must be a number`);
+    }
+
+    return value.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  }
+  
+  describe('numberToCurrency', () => {
+    describe('given an invalid number', () => {
+      test('composed of non-numbers to throw error', () => {
+        expect(() => numberToCurrency('abc')).toThrow();
+      });
+    });
+  
+    describe('given a valid number', () => {
+      test('returns the correct string format', () => {
+        expect(numberToCurrency(10000)).toBe('10,000.00');
+      });
+    });
+  });
+
