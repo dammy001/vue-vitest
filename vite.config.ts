8a0b2f7f305c71d744dc4f52f4a083bf2138756f
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-01-22 07:28:50 +0100

    chore: eslint & prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [vue()],
- test: {
-  global: true,
-  environment: 'happy-dom',
-  coverage: {
-   reporter: ['text', 'html'],
+  resolve: {
+    alias: {
+      '@/': `${resolve(__dirname, './src')}`,
+    },
   },
- },
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.ts$/],
+    }),
+  ],
+  test: {
+    global: true,
+    environment: 'happy-dom',
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+  },
 });
